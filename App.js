08f1ba3885852cfd18dfef004ce5d82d58ad258c
Daniele Esposito <danyesp5@gmail.com>
2023-02-10 19:04:23 +0100

    wip

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useCallback } from 'react';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        'Archivo-Regular': require('./assets/fonts/Archivo-Regular.ttf'),
+        'Archivo-Bold': require('./assets/fonts/Archivo-Bold.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+        <View style={{flex: 1}} onLayout={onLayoutRootView}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </View>
     );
 }
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,19 +1,111 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, Dimensions } from "react-native";
+import {
+    SafeAreaView
+  } from 'react-native-safe-area-context';
+
+const logoImage = require('../assets/images/logo.png');
+const screenWidth = Dimensions.get("window").width;
+var cardWidth = screenWidth > 2000 ? 450 : (screenWidth / 3) - 40; 
+var logoWidth = screenWidth > 2000 ? 800 : (screenWidth / 2) - 100; 
 
 export default function HomeScreen({navigation}) {
+
+    useEffect(() => {
+        console.log(logoWidth);
+      }, []);
+
     return (
-        <View style={styles.container}>
-            <Text>Home Screen</Text>
-        </View>
+            <View className="flex-1 bg-primary-50 px-5">
+                <SafeAreaView style={{flex: 1}}>
+                <ScrollView style={{ height: "100%" }} >
+                    <View className="pt-4">
+                        <View className="flex flex-row items-center justify-between gap-12">
+                            <View className="overflow-hidden" >
+                                <Image source={logoImage} style={{width: logoWidth, height: logoWidth / 2}} resizeMode="contain" />
+                            </View>
+                            <View className="flex">
+                                <Text className=" text-5xl uppercase text-left text-white" style={{ fontFamily: 'Archivo-Bold'}}>acquista</Text>
+                                <Text className=" text-5xl uppercase text-left text-white" style={{ fontFamily: 'Archivo-Bold'}}>i nostri</Text>
+                                <Text className=" text-5xl uppercase text-left text-white" style={{ fontFamily: 'Archivo-Bold'}}>spettacoli</Text>
+                            </View>
+                        </View>
+
+                        
+                        
+                        <View className="pt-8">
+                            <Text className="text-4xl text-white" style={{ fontFamily: 'Archivo-Bold'}}>Spettacoli in evidenza</Text>
+                            <ScrollView horizontal={true} style={{ paddingBottom: 30 }} showsHorizontalScrollIndicator={false}>
+                                {
+                                    spettacoli.map((card, index) => (
+                                        <TouchableOpacity key={index} onPress={() => {
+                                                console.log('ciao');
+                                            }}>
+                                            <View className="pt-8 mr-5 overflow-hidden w-full" style={{width: cardWidth, height: cardWidth * 2, overflow: 'hidden'}}>
+                                                <Image source={card.image} style={{width: cardWidth, height: cardWidth * 2, resizeMode: 'cover', overflow: 'hidden'}} resizeMode="cover"/>
+                                            </View>
+                                        </TouchableOpacity>
+                                    ))
+                                }
+                                </ScrollView>
+                        </View>
+
+                        <View className="pt-8">
+                            <Text className="text-4xl text-white" style={{ fontFamily: 'Archivo-Bold'}}>Prossimi appuntamenti</Text>
+                            <ScrollView horizontal={true} style={{ paddingBottom: 30 }} showsHorizontalScrollIndicator={false}>
+                                {
+                                    spettacoli.map((card, index) => (
+                                        <TouchableOpacity key={index} onPress={() => {
+                                                console.log('ciao');
+                                            }}>
+                                            <View className="pt-8 mr-5 overflow-hidden w-full" style={{width: cardWidth, height: cardWidth * 2, overflow: 'hidden'}}>
+                                                <Image source={card.image} style={{width: cardWidth, height: cardWidth * 2, resizeMode: 'cover', overflow: 'hidden'}} resizeMode="cover"/>
+                                            </View>
+                                        </TouchableOpacity>
+                                    ))
+                                }
+                                </ScrollView>
+                        </View>
+
+                        <View className="pt-8">
+                            <Text className="text-4xl text-white" style={{ fontFamily: 'Archivo-Bold'}}>Coming soon</Text>
+                            <ScrollView horizontal={true} style={{ paddingBottom: 30 }} showsHorizontalScrollIndicator={false}>
+                                {
+                                    spettacoli.map((card, index) => (
+                                        <TouchableOpacity key={index} onPress={() => {
+                                                console.log('ciao');
+                                            }}>
+                                            <View className="pt-8 mr-5 overflow-hidden w-full" style={{width: cardWidth, height: cardWidth * 2, overflow: 'hidden'}}>
+                                                <Image source={card.image} style={{width: cardWidth, height: cardWidth * 2, resizeMode: 'cover', overflow: 'hidden'}} resizeMode="cover"/>
+                                            </View>
+                                        </TouchableOpacity>
+                                    ))
+                                }
+                                </ScrollView>
+                        </View>
+                        
+                    </View>
+                </ScrollView>
+                </SafeAreaView>
+            </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+const spettacoli = [
+    {
+        title: "IL MEDICO DEI PAZZI",
+        image: require("../assets/test.jpg")
     },
-});
+    {
+        title: "Styled Components",
+        image: require("../assets/test.jpg")
+    },
+    {
+        title: "Props and Icons",
+        image: require("../assets/test.jpg")
+    },
+    {
+        title: "Static Data and Loop",
+        image: require("../assets/test.jpg")
+    }
+];
